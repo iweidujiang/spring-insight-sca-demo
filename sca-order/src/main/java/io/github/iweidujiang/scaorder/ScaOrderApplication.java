d19cc03fbc322a35c780f801a2c9bc2ce833e08f
chenpeng <1913127929@qq.com>
2026-01-18 22:13:35 +0800

    增加子模块

--- sca-order/src/main/java/io/github/iweidujiang/scaorder/ScaOrderApplication.java
+++ sca-order/src/main/java/io/github/iweidujiang/scaorder/ScaOrderApplication.java
@@ -1,8 +1,14 @@
 package io.github.iweidujiang.scaorder;
 
+import io.github.iweidujiang.springinsight.annotation.EnableSpringInsight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableSpringInsight
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class ScaOrderApplication {
 
--- sca-user/src/main/java/io/github/iweidujiang/scauser/ScaUserApplication.java
+++ sca-user/src/main/java/io/github/iweidujiang/scauser/ScaUserApplication.java
@@ -1,8 +1,12 @@
 package io.github.iweidujiang.scauser;
 
+import io.github.iweidujiang.springinsight.annotation.EnableSpringInsight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableSpringInsight
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ScaUserApplication {
 
